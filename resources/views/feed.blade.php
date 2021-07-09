ee48267c0c8c3773cda9ce3811ebfbaa19426bd7
Trần Minh Hiếu <hieuleggo1280@gmail.com>
2021-07-09 15:41:48 +0700

    complete header

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -41,7 +41,10 @@
                         <div class="header-top-navigation">
                             <nav>
                                 <ul>
+                                    <!-- Home -->
                                     <li class="active"><a href="/home"><i class="fas fa-home" style="font-size: 30px; color: #5271ff"></i></a></li>
+
+                                    <!-- Message -->
                                     <li class="msg-trigger">
                                         <a href="#message" class="msg-trigger-btn"><i class="fab fa-facebook-messenger" style="font-size: 30px; color: #5271ff"></i></a>
                                         <div class="message-dropdown" id="message">
@@ -54,16 +57,116 @@
                                             </div>
                                             <ul class="dropdown-msg-list">
                                                 <li class="msg-list-item d-flex justify-content-between">
+                                                    <!-- profile picture -->
                                                     <div class="profile-thumb">
                                                         <figure class="profile-thumb-middle">
                                                             <img src="/assets/img/sample_ava1.jpg" alt="profile picture">
                                                         </figure>
                                                     </div>
+
+                                                    <!-- message content -->
+                                                    <div class="msg-content">
+                                                        <h6 class="author"><a href="#" style="text-decoration: none">Dương Lê Phước Trung</a></h6>
+                                                        <p class="msg-property">
+                                                            Nói cái gì đó nhưng không biết nhưng ghi vô cho nó dài.
+                                                        </p>
+                                                    </div>
+
+                                                    <!-- message time -->
+                                                    <div class="msg-time">
+                                                        <p>08 Jul 2021</p>
+                                                    </div>
                                                 </li>
+                                            </ul>
+                                            <ul class="dropdown-msg-list">
+                                                <li class="msg-list-item d-flex justify-content-between">
+                                                    <!-- profile picture -->
+                                                    <div class="profile-thumb">
+                                                        <figure class="profile-thumb-middle">
+                                                            <img src="/assets/img/sample_ava2.jpg" alt="profile picture">
+                                                        </figure>
+                                                    </div>
+
+                                                    <!-- message content -->
+                                                    <div class="msg-content">
+                                                        <h6 class="author"><a href="#" style="text-decoration: none">Tài Trần</a></h6>
+                                                        <p class="msg-property">
+                                                            Nói cái gì đó nhưng không biết nhưng ghi vô cho nó dài.
+                                                        </p>
+                                                    </div>
+
+                                                    <!-- message time -->
+                                                    <div class="msg-time">
+                                                        <p>08 Jul 2021</p>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <ul id="all-message">
+                                                <a href="#" style="text-decoration: none; font-size: 14px">Xem tất cả Tin Nhắn</a>
                                             </ul>
                                         </div>
                                     </li>
-                                    <li class="active"><a href="#"><i class="fas fa-bell" style="font-size: 30px; color: #5271ff"></i></a></li>
+
+                                    <!-- Notification -->
+                                    <li class="notification-trigger">
+                                        <a href="#notification" class="msg-trigger-btn"><i class="fas fa-bell" style="font-size: 30px; color: #5271ff"></i></a>
+                                        <div class="message-dropdown" id="notification">
+                                            <div class="dropdown-title">
+                                                <p class="recent-msg">Thông báo</p>
+                                            </div>
+                                            <ul class="dropdown-msg-list">
+                                                <li class="msg-list-item d-flex justify-content-between">
+                                                    <!-- profile picture -->
+                                                    <div class="profile-thumb">
+                                                        <figure class="profile-thumb-middle">
+                                                            <img src="/assets/img/sample_ava1.jpg" alt="profile picture">
+                                                        </figure>
+                                                    </div>
+
+                                                    <!-- message content -->
+                                                    <div class="msg-content notification-content">
+                                                        <a href="#" class="author" style="text-decoration: none">
+                                                            <strong>Dương Lê Phước Trung</strong> và những người khác
+                                                        </a>
+                                                        <p class="msg-property">
+                                                            Đã thích ảnh của bạn.
+                                                        </p>
+                                                    </div>
+
+                                                    <!-- message time -->
+                                                    <div class="msg-time">
+                                                        <p>08 Jul 2021</p>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <ul class="dropdown-msg-list">
+                                                <li class="msg-list-item d-flex justify-content-between">
+                                                    <!-- profile picture -->
+                                                    <div class="profile-thumb">
+                                                        <figure class="profile-thumb-middle">
+                                                            <img src="/assets/img/sample_ava2.jpg" alt="profile picture">
+                                                        </figure>
+                                                    </div>
+
+                                                    <!-- message content -->
+                                                    <div class="msg-content notification-content">
+                                                        <h6 class="author"><a href="#" style="text-decoration: none">Tài Trần</a></h6>
+                                                        <p class="msg-property">
+                                                            Đã bình luận vào bài viết của bạn.
+                                                        </p>
+                                                    </div>
+
+                                                    <!-- message time -->
+                                                    <div class="msg-time">
+                                                        <p>08 Jul 2021</p>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <ul id="all-notification">
+                                                <a href="#" style="text-decoration: none; font-size: 14px">Xem tất cả Thông Báo</a>
+                                            </ul>
+                                        </div>
+                                    </li>
                                 </ul>
                             </nav>
                         </div>
@@ -90,6 +193,19 @@
                                             <img src="/assets/img/avartar.png" alt="profile picture">
                                         </figure>
                                     </a>
+                                    <div class="profile-dropdown">
+                                        <div class="profile-head">
+                                            <h5 class="name"><a href="#">Minh Hiếu</a></h5>
+                                            <a class="mail" href="#">hieutran@gmail.com</a>
+                                        </div>
+                                        <div class="profile-body">
+                                            <ul>
+                                                <li><a href="#"><i class="fas fa-user" style="font-size: 16px; color: #5271ff"></i>Trang cá nhân</a></li>
+                                                <li><a href="#"><i class="fas fa-cog" style="font-size: 16px; color: #5271ff"></i>Cài đặt</a></li>
+                                                <li><a href="/"><i class="fas fa-sign-out-alt" style="font-size: 16px; color: #5271ff"></i>Đăng xuất</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
